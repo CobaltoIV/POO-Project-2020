0f0d53ed7d61b9e92307e38402d72314713c34f8
Francisco Carvalho <quelincho@gmail.com>
2020-05-04 02:45:52 +0100

    input handler first iteration

--- Testcode/InputHandler.java
+++ Testcode/InputHandler.java
@@ -1,9 +1,36 @@
-public  class InputHandler{
+import java.io.*;
+import java.util.*;
 
-    // Map Data
+public class InputHandler {
+    public static void main(String[] args) {
+        String line;
+        String splitToken = ",";
+        Map<String, ArrayList<Integer>> map = new HashMap<String, ArrayList<Integer>>();
+        ArrayList<String> labels = new ArrayList<String>();
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(args[0]));
+            line = br.readLine();
+            String[] tmpLabels = line.split(splitToken);
+            for(String string : tmpLabels)
+                labels.add(string);
 
+            while ((line = br.readLine()) != null)
+            {
+                String[] instance = line.split(splitToken);
+                for (int i = 0; i < labels.size(); i++){
+                    if (map.get(tmpLabels[i]) == null)
+                        map.put(tmpLabels[i], new ArrayList<Integer>());
+                    map.get(tmpLabels[i]).add(Integer.parseInt(instance[i]));
+                }
+                
+            }
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-    // Map read_csv(String filename)
-
-
+        for (String string : labels) {
+            System.out.println(map.get(string));
+        }
+    }
 }
--- Testcode/scoring/alpha.java
+++ Testcode/scoring/alpha.java
@@ -1,41 +1,43 @@
 package scoring;
 
-public class alpha{
+public class alpha {
 
     private double weigth;
-    private int [][][] Source;
+    private int[][][] Source;
 
-    public double getWeigth(){
-       return this.weigth;
+    public double getWeigth() {
+        return this.weigth;
     }
-    public int [][][]getSource(){
+
+    public int[][][] getSource() {
         return this.Source;
     }
-    public double setWeigth(double weigth){
+
+    public double setWeigth(double weigth) {
         return this.weigth = weigth;
     }
-    public int[][][] setSource (int [][][] Source){
+
+    public int[][][] setSource(int[][][] Source) {
         return this.Source = Source;
     }
-    
-    
-    public int[][][] calcN(int[] parent, int[] parentvalues,int[] son, int[] sonvalues , int[] classes, int[] classvalues)
-    {
-       
-        int [][][] N_jkc = new int[parentvalues.length][sonvalues.length][classvalues.length];
 
-        for (int d=0;  d< parent.length ;d++){
+    public int[][][] calcN(int[] parent, int[] parentvalues, int[] son, int[] sonvalues, int[] classes,
+            int[] classvalues) {
 
-            for (int k=0; k<sonvalues.length; k++){
+        int[][][] N_jkc = new int[parentvalues.length][sonvalues.length][classvalues.length];
 
-                for (int j=0;j< parentvalues.length;j++){
-                   
-                    for (int c = 0; c< classvalues.length;c++){
-                     
-                       if (parent[d] == parentvalues[j] && son[d] == sonvalues[k] && classes[d] == classvalues[c])
-                       N_jkc[j][k][c]++;
+        for (int d = 0; d < parent.length; d++) {
+
+            for (int k = 0; k < sonvalues.length; k++) {
+
+                for (int j = 0; j < parentvalues.length; j++) {
+
+                    for (int c = 0; c < classvalues.length; c++) {
+
+                        if (parent[d] == parentvalues[j] && son[d] == sonvalues[k] && classes[d] == classvalues[c])
+                            N_jkc[j][k][c]++;
                     }
-                }          
+                }
 
             }
 
