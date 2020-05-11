4a485157a1c9e11d52518ceba10d4b2ea313858e
CobaltoIV <62718484+CobaltoIV@users.noreply.github.com>
2020-05-11 16:57:28 +0100

    For extension

---
+++ Code/classifier/Classifiers.java
@@ -0,0 +1,15 @@
+package classifier;
+
+import evaluation.Scorer;
+import java.util.*;
+import data.*;
+
+public abstract class Classifiers implements Scorer{
+
+    public ArrayList<Integer> predict(InputHandler data) {
+
+        return new ArrayList<Integer>();
+    }
+
+
+}
