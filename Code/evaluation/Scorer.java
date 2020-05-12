4a2e9919eb4c5745c8ff9c187171524ab3a7d5f7
CobaltoIV <62718484+CobaltoIV@users.noreply.github.com>
2020-05-12 18:24:34 +0100

    alterin

--- Code/evaluation/Scorer.java
+++ Code/evaluation/Scorer.java
@@ -2,23 +2,83 @@ package evaluation;
 
 import java.util.ArrayList;
 
+/**
+ * The interface Scorer contains the functions t evaluate the classifier
+ * performance.
+ */
 public interface Scorer {
 
-    public ArrayList<int[][]> calcConfusionMatrix(ArrayList<Integer> predictions, ArrayList<Integer> real,
-            int n_unique);
+        /**
+         * Calculates the Confusions matrixes taking each possible value of the class as
+         * positive
+         * 
+         * @param predictions  predicted values
+         * @param real         actual values
+         * @param n_unique     number of unique values
+         * @return Confusion Matrix for each value
+         */
+        public ArrayList<int[][]> calcConfusionMatrix(ArrayList<Integer> predictions, ArrayList<Integer> real,
+                        int n_unique);
 
-    public ArrayList<Double> calc_Sens(ArrayList<int[][]> conf_list, double[] N_C, int N);
+        /**
+         * Calculates the sensitivy for each confusion matrix
+         * @param conf_list  list of confusion matrixes
+         * @param N_C {@link scoring.alpha#_N_C} 
+         * @param N  {@link scoring.alpha#_N} 
+         * @return
+         */
+        public ArrayList<Double> calc_Sens(ArrayList<int[][]> conf_list, double[] N_C, int N);
 
-    public ArrayList<Double> calc_Spec(ArrayList<int[][]> conf_list, double[] N_C, int N);
+        /**
+         * Calculates the specificity for each confusion matrix
+         * @param conf_list  list of confusion matrixes
+         * @param N_C {@link scoring.alpha#_N_C} 
+         * @param N  {@link scoring.alpha#_N} 
+         * @return
+         */
+        public ArrayList<Double> calc_Spec(ArrayList<int[][]> conf_list, double[] N_C, int N);
 
-    public ArrayList<Double> calc_F1(ArrayList<int[][]> conf_list, double[] N_C, int N);
+        /**
+         * Calculates the F1 Score for each confusion matrix
+         * @param conf_list  list of confusion matrixes
+         * @param N_C {@link scoring.alpha#_N_C} 
+         * @param N  {@link scoring.alpha#_N} 
+         * @return
+         */
+        public ArrayList<Double> calc_F1(ArrayList<int[][]> conf_list, double[] N_C, int N);
 
-    public double calc_Acc(ArrayList<Integer> predictions, ArrayList<Integer> real);
+        /**
+         * Calculates accuracy for each confusion matrix
+         * @param predictions predicted values 
+         * @param real real values
+         * @return
+         */
+        public double calc_Acc(ArrayList<Integer> predictions, ArrayList<Integer> real);
 
-    public void printMetrics(Double Acc, ArrayList<Double> Sens, ArrayList<Double> Spec, ArrayList<Double> F1);
+        /**
+         * Prints metrics
+         * @param Acc Accuracy
+         * @param Sens Sensitivity
+         * @param Spec Specificity
+         * @param F1 F1 Score
+         */
+        public void printMetrics(Double Acc, ArrayList<Double> Sens, ArrayList<Double> Spec, ArrayList<Double> F1);
 
-    public void measurePerformance(ArrayList<Integer> predictions, ArrayList<Integer> real, int n_unique, double[] N_C,
-            int N);
-    public void printPredictions(ArrayList<Integer> predictions);
+        /**
+         * Method that organizes all the methods before
+         * @param predictions
+         * @param real
+         * @param n_unique
+         * @param N_C
+         * @param N
+         */
+        public void measurePerformance(ArrayList<Integer> predictions, ArrayList<Integer> real, int n_unique,
+                        double[] N_C, int N);
+
+        /**
+         * Prints predictions
+         * @param predictions
+         */
+        public void printPredictions(ArrayList<Integer> predictions);
 
 }
--- Code/evaluation/package-info.java
+++ Code/evaluation/package-info.java
@@ -1,1 +1,14 @@
+/**
+ * This package contains the one interface {@link evaluation.Scorer}.
+ * The interface is to be implemented by classifiers, it provides the function
+ * to calculate the following accuracy measures:
+ * <li> Accuracy
+ * <li> Sensitivity
+ * <li> F1 Score
+ * <li> Specificity
+ * <p>
+ * It also contains the function
+ *  {@link evaluation.Scorer#printMetrics(Double, java.util.ArrayList, java.util.ArrayList, java.util.ArrayList)}
+ * which prints all the metrics in a pre determined way.
+ */
 package evaluation;
