c791981085d3a25cfc387047024b213971695066
Francisco Quelincho <quelincho@gmail.com>
2020-05-13 15:42:35 +0100

    fixed scorer comments that had older variables

--- Code/evaluation/Scorer.java
+++ Code/evaluation/Scorer.java
@@ -32,8 +32,6 @@ public interface Scorer {
         /**
          * Calculates the specificity for each confusion matrix
          * @param conf_list  list of confusion matrixes
-         * @param N_C {@link scoring.alpha#_N_C}
-         * @param N  {@link scoring.alpha#_N}
          * @return
          */
         public ArrayList<Double> calc_Spec(ArrayList<int[][]> conf_list);
@@ -41,8 +39,6 @@ public interface Scorer {
         /**
          * Calculates the F1 Score for each confusion matrix
          * @param conf_list  list of confusion matrixes
-         * @param N_C {@link scoring.alpha#_N_C}
-         * @param N  {@link scoring.alpha#_N}
          * @return
          */
         public ArrayList<Double> calc_F1(ArrayList<int[][]> conf_list);
@@ -69,8 +65,6 @@ public interface Scorer {
          * @param predictions
          * @param real
          * @param n_unique
-         * @param N_C
-         * @param N
          */
         public void measurePerformance(ArrayList<Integer> predictions, ArrayList<Integer> real, int n_unique);
 
